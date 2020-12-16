f81d1d466ad5544ebd3763be9ba7a943fca65ec9
Alesya90 <alesyazhuravlik@gmail.com>
2020-12-16 09:17:24 +0500

    3. Интерфейс Input [#402439]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void go() {
-
+        System.out.println("Поехали");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Число пассажиров " + count);
     }
 
     @Override
     public float azs(float petrol) {
+        System.out.println("Вы заправили " + petrol + " л топлива");
         return 0;
     }
 }
