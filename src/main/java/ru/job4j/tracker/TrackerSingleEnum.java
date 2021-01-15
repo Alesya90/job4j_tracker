e579b4f108a0567e73267dc08e6cfbff75e397f5
Alesya90 <alesyazhuravlik@gmail.com>
2021-01-15 13:07:37 +0500

    2. Singleton. [#402473]

--- src/main/java/ru/job4j/tracker/TrackerSingleEnum.java
+++ src/main/java/ru/job4j/tracker/TrackerSingleEnum.java
@@ -2,4 +2,10 @@ package ru.job4j.tracker;
 
 public enum TrackerSingleEnum {
         INSTANCE;
+
+    private Tracker tracker  = new Tracker();
+
+    public Tracker getInstance() {
+        return tracker;
+    }
 }
